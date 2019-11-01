a57892f824d298388cf06596229ec0021809be24
Massimo Donati <massimo.donati@doordash.com>
2019-11-01 11:47:06 -0700

    fix trevis config

--- fastlane/utility.rb
+++ fastlane/utility.rb
@@ -1,4 +1,4 @@
 
 def test_on_mac()
-    sh("xcodebuild test -project ../Tambo.xcodeproj -scheme 'Tambo macOS' -destination 'platform=OS X,arch=x86_64'")
+    sh("xcodebuild -project ../Tambo.xcodeproj -scheme 'Tambo macOS' -destination 'platform=OS X,arch=x86_64' build test")
 end
