534e70053a010b629dbd19d364c6e3bff2285bf4
Massimo Donati <mass.donati@gmail.com>
2020-06-04 22:34:46 -0700

    fix package.swift

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Tambo",
-            dependencies: []),
+            
+            dependencies: [], path: "Tambo"),
         .testTarget(
             name: "TamboTests",
-            dependencies: ["Tambo"]),
+            
+            dependencies: ["Tambo"], path: "TamboTests"),
     ]
 )
